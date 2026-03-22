c6c6338c99e6bf6f343cba6f11dd6c1c856adafe
YanisWenger <yanis.wenger@free.fr>
2026-03-22 15:45:32 +0100

    Add main file

--- Heisenberg_Main.jl
+++ Heisenberg_Main.jl
@@ -8,7 +8,7 @@ T=Temperatures(.65,1.15,8)        # Temperatures
 d=Float32.([-1,1])              # anisotropic term
 PBC   = true                    # Periodic Boundary Conditions
 pi32  = Float32(π)
-Save  = true
+Save  = true                    # to save Data in a folder named L_N (that needs to be created before, i.e.: [8, 12, 20]_1000000
 
 println("  --  $N sweeps  --  ")
 starttime = time()
@@ -27,6 +27,7 @@ println(t)
 
 # C, Susc and Corr max
 
-# For the real big simulations, which T should I care of?  Is is good to have a rough simulations to then see at which T we should simulate for all L and d
+# For the real big simulations, which T should I care of?  Is is good to have a rough simulations to then see at which T we should simulate for all L and d?
+# As we can see for the last simulations, it is not useful to have 40T with "only" 1M lattice sweeps. So maybe we can take 32, such that I can do the simulations on almost any computer of the cluster
 
 # Correlation : actually only correlation in rows, for all the rows and all element in a row with its L/2-1 neighbor (in each side)
--- Heisenberg_Main_functions.jl
+++ Heisenberg_Main_functions.jl
@@ -22,7 +22,7 @@ function Energy(Lattice::Array{Float32, 3}, L::Int, PBC::Bool, d::Float32)    # 
     return E
 end #   @inbounds almost divide the execution time by 2
 
-@inline function Correlation(ϕ1::Float32, θ1::Float32, ϕ2::Float32, θ2::Float32)
+@inline @fastmath function Correlation(ϕ1::Float32, θ1::Float32, ϕ2::Float32, θ2::Float32)
     sin(θ1)*sin(θ2)*cos(ϕ1-ϕ2) + cos(θ1)*cos(θ2)
 end
 
@@ -33,26 +33,26 @@ function Initial_lattice(L::Int, pi32::Float32)
     return lattice
 end
 
-@inline function EnergyDiff(spin0old::Vector{Float32}, spin0new::Vector{Float32}, spin1::Vector{Float32}, spin2::Vector{Float32}, spin3::Vector{Float32}, spin4::Vector{Float32}, d::Float32) # calculate the bounding energy difference between a spin and its neigbors and another one with the same neighbors
+@inline @fastmath function EnergyDiff(spin0old::Vector{Float32}, spin0new::Vector{Float32}, spin1::Vector{Float32}, spin2::Vector{Float32}, spin3::Vector{Float32}, spin4::Vector{Float32}, d::Float32) # calculate the bounding energy difference between a spin and its neigbors and another one with the same neighbors
     sold, snew = sin(spin0old[2]), sin(spin0new[2])
     cold, cnew = cos(spin0old[2]), cos(spin0new[2])
     return (snew*cos(spin0new[1]-spin1[1]) - sold*cos(spin0old[1]-spin1[1]))*sin(spin1[2]) + (cnew-cold)*cos(spin1[2])  +  (snew*cos(spin0new[1]-spin2[1]) - sold*cos(spin0old[1]-spin2[1]))*sin(spin2[2]) + (cnew-cold)*cos(spin2[2])  +  (snew*cos(spin0new[1]-spin3[1]) - sold*cos(spin0old[1]-spin3[1]))*sin(spin3[2]) + (cnew-cold)*cos(spin3[2])  +  (snew*cos(spin0new[1]-spin4[1]) - sold*cos(spin0old[1]-spin4[1]))*sin(spin4[2]) + (cnew-cold)*cos(spin4[2])   +   d*(cold^2-cnew^2) # old - new for the last term, as H = - Σ Si Sj + d Σ Sz^2 (sign difference between these two terms)
 end
 
-@inline function EnergyDiffTheta(spin0old::Vector{Float32}, theta::Float32, spin1::Vector{Float32}, spin2::Vector{Float32}, spin3::Vector{Float32}, spin4::Vector{Float32}, d::Float32) # Like EnergyDiff but optimized for a change in theta only (for XY-theta sweep or Ising flip)
+@inline @fastmath function EnergyDiffTheta(spin0old::Vector{Float32}, theta::Float32, spin1::Vector{Float32}, spin2::Vector{Float32}, spin3::Vector{Float32}, spin4::Vector{Float32}, d::Float32) # Like EnergyDiff but optimized for a change in theta only (for XY-theta sweep or Ising flip)
     sold, snew = sin(spin0old[2]), sin(theta)
     cold, cnew = cos(spin0old[2]), cos(theta)
     phi = spin0old[1]
     return (snew-sold)*(cos(phi-spin1[1])*sin(spin1[2]) + cos(phi-spin2[1])*sin(spin2[2]) + cos(phi-spin3[1])*sin(spin3[2]) + cos(phi-spin4[1])*sin(spin4[2])) + (cnew-cold)*(cos(spin1[2])+cos(spin2[2])+cos(spin3[2])+cos(spin4[2]))   +   d*(cold^2-cnew^2)
 end
 
-@inline function EnergyDiffPhi(spin0old::Vector{Float32}, phi::Float32, spin1::Vector{Float32}, spin2::Vector{Float32}, spin3::Vector{Float32}, spin4::Vector{Float32}) #  Like EnergyDiff but optimized for a change in phi only (for XY-phi sweep)
+@inline @fastmath function EnergyDiffPhi(spin0old::Vector{Float32}, phi::Float32, spin1::Vector{Float32}, spin2::Vector{Float32}, spin3::Vector{Float32}, spin4::Vector{Float32}) #  Like EnergyDiff but optimized for a change in phi only (for XY-phi sweep)
     phiold = spin0old[1]
     return sin(spin0old[2])*((cos(phi-spin1[1])-cos(phiold-spin1[1]))*sin(spin1[2])  +  (cos(phi-spin2[1])-cos(phiold-spin2[1]))*sin(spin2[2])  +  (cos(phi-spin3[1])-cos(phiold-spin3[1]))*sin(spin3[2])  +  (cos(phi-spin4[1])-cos(phiold-spin4[1]))*sin(spin4[2]))
 end
 
-function SingleFlip(Lattice::Array{Float32, 3}, i::Int, j::Int, T::Float32, L::Int, σ::Float32, σϕ::Float32, σθ::Float32, pi32::Float32, PBC::Bool, d::Float32, pIsing::Float32, pXY::Float32)      # Try a new configuration (proposal) and either accept or reject it
-    newspin, Whichflip = NewSpin(Lattice[:,i,j], σ, σϕ, σθ, pi32, pIsing, pXY)
+function SingleFlip(Lattice::Array{Float32, 3}, i::Int, j::Int, T::Float32, L::Int, σ::Float32, σϕ::Float32, σθ::Float32, pi32::Float32, PBC::Bool, d::Float32, pIsing::Float32, pXY::Float32, rng::AbstractRNG)      # Try a new configuration (proposal) and either accept or reject it
+    newspin, Whichflip = NewSpin(Lattice[:,i,j], σ, σϕ, σθ, pi32, pIsing, pXY, rng)
     if PBC==false
         Δ = 0f0
         if i!=1
@@ -88,44 +88,43 @@ function SingleFlip(Lattice::Array{Float32, 3}, i::Int, j::Int, T::Float32, L::I
     return Lattice[:,i,j], acceptance, Whichflip
 end
 
-function NewSpin(spin::Vector{Float32}, σ::Float32, σϕ::Float32, σθ::Float32, pi32::Float32, pIsing::Union{Float32, Int}, pXY::Union{Float32, Int}) # Choose which kind of proposal (isotropic one, Ising flip, only change phi, only change theta)
+function NewSpin(spin::Vector{Float32}, σ::Float32, σϕ::Float32, σθ::Float32, pi32::Float32, pIsing::Union{Float32, Int}, pXY::Union{Float32, Int}, rng::AbstractRNG) # Choose which kind of proposal (isotropic one, Ising flip, only change phi, only change theta)
     if pIsing == 0 && pXY == 0
         Whichflip = 1
-        spin = NewPhiTheta(spin, σ, pi32)
+        spin = NewPhiTheta(spin, σ, pi32, rng)
     else
-        random = rand(TaskLocalRNG())       # This way to generate less random number, thus faster code
+        random = rand(rng)       # This way to generate less random number, thus faster code
         if pIsing > 0 && random < pIsing
             Whichflip = 2
             spin[2] = pi32 - spin[2]
         elseif random < pXY
             if random > .3*pXY
-                spin[1] = NewPhi(spin[1], σϕ, pi32)
+                spin[1] = NewPhi(spin[1], σϕ, pi32, rng)
                 Whichflip = 3
             else
-                spin[2] = NewTheta(spin[2], σθ, pi32)
+                spin[2] = NewTheta(spin[2], σθ, pi32, rng)
                 Whichflip = 4
             end
         else                                # Same thing than the first part of the function, to lose less time (as generate a random number takes way more time than an if / else
             Whichflip = 1
-            spin = NewPhiTheta(spin, σ, pi32)
+            spin = NewPhiTheta(spin, σ, pi32, rng)
         end
     end
     return spin, Whichflip
 end
 
-@inline function NewPhi(phi::Float32, σϕ::Float32, pi32::Float32) # Propose a new phi
-    mod(rand(TaskLocalRNG(), Normal(phi, σϕ)), 2*pi32)
+@inline function NewPhi(phi::Float32, σϕ::Float32, pi32::Float32, rng::AbstractRNG) # Propose a new phi
+    mod(rand(rng, Normal(phi, σϕ)), 2*pi32)
 end
 
-@inline function NewTheta(theta::Float32, σθ::Float32, pi32::Float32) # Propose a new Theta
-    theta = mod(rand(TaskLocalRNG(), Normal(theta, σθ)), 2*pi32)
+@inline function NewTheta(theta::Float32, σθ::Float32, pi32::Float32, rng::AbstractRNG) # Propose a new Theta
+    theta = mod(rand(rng, Normal(theta, σθ)), 2*pi32)
     if theta > pi32; theta = 2*pi32 - theta
     end
     return theta
 end
 
-function NewPhiTheta(spin::Vector{Float32}, σ::Float32, pi32::Float32) # Propose new phi and theta (isotropic sweep)
-    rng = TaskLocalRNG()
+function NewPhiTheta(spin::Vector{Float32}, σ::Float32, pi32::Float32, rng::AbstractRNG) # Propose new phi and theta (isotropic sweep)
     alpha  = rand(rng, Float32)*2*pi32
     @fastmath begin
         beta   = min(abs(rand(rng, Normal(0f0, σ))), pi32)
@@ -140,17 +139,17 @@ function NewPhiTheta(spin::Vector{Float32}, σ::Float32, pi32::Float32) # Propos
     return spin
 end
 
-function LatticeSweep(Lattice::Array{Float32, 3}, L::Int, E::Float32, T::Float32, σ::Float32, σϕ::Float32, σθ::Float32, pi32::Float32, PBC::Bool, d::Float32, pIsing::Float32, pXY::Float32)    #MH lattice sweep
+function LatticeSweep(Lattice::Array{Float32, 3}, L::Int, T::Float32, σ::Float32, σϕ::Float32, σθ::Float32, pi32::Float32, PBC::Bool, d::Float32, pIsing::Float32, pXY::Float32, rng::AbstractRNG)    #MH lattice sweep
     Accept, Tr = zeros(Int, 4), zeros(Int, 4)
-    for j in 1:L, k in 1:L
-        Lattice[:,j,k], a, Whichflip = SingleFlip(Lattice,j,k,T,L,σ, σϕ, σθ, pi32, PBC, d, pIsing, pXY)
+    @inbounds for j in 1:L, k in 1:L
+        Lattice[:,j,k], a, Whichflip = SingleFlip(Lattice,j,k,T,L,σ, σϕ, σθ, pi32, PBC, d, pIsing, pXY, rng)
         Accept[Whichflip]+=a
         Tr[Whichflip]+=1
     end
     return Lattice, Accept, Tr
 end
 
-function MultipleIsingFlips(Lattice::Array{Float32, 3}, L::Int, T::Float32, pi32::Float32, d::Float32, PBC::Bool)      # Wolff update
+function MultipleIsingFlips(Lattice::Array{Float32, 3}, L::Int, T::Float32, pi32::Float32, d::Float32, PBC::Bool, rng::AbstractRNG)      # Wolff update
     p = 1 - ℯ^(-2/T)    # what should we do about d ?
     k, l = rand(TaskLocalRNG(), 1:L, 2)
     InCluster = falses(L,L)
@@ -158,21 +157,25 @@ function MultipleIsingFlips(Lattice::Array{Float32, 3}, L::Int, T::Float32, pi32
     CheckNeighbor = [[k,l]]
     while CheckNeighbor != []
         i,j = pop!(CheckNeighbor)
-        NewCheckNeighbor, InCluster = AddNeighbors(i,j, L, InCluster, p, Lattice[2,:,:],pi32) # check first i-1,j; then i,j-1; i+1,j and finally i,j+1
+        NewCheckNeighbor, InCluster = AddNeighbors(i,j, L, InCluster, p, Lattice[2,:,:],pi32, rng) # check first i-1,j; then i,j-1; i+1,j and finally i,j+1
         CheckNeighbor = unique(vcat(CheckNeighbor,NewCheckNeighbor))
     end
-    Lattice[2,:,:] = InCluster .* (pi32 .- Lattice[2,:,:]) .+ .!InCluster .* Lattice[2,:,:]
+    @inbounds for i in 1:L, j in 1:L
+        if InCluster[i,j]
+            Lattice[2,i,j] = pi32 - Lattice[2,i,j]
+        end
+    end
     E = Energy(Lattice, L, PBC, d)
     return Lattice, E
 end
 
-function AddNeighbors(i::Int,j::Int, L::Int, InCluster::BitMatrix, p::Float32, ZLattice::Array{Float32, 2}, pi32::Float32)    # For Wolff update: add or not to the cluster the neighbors of a site already in the cluster
+function AddNeighbors(i::Int,j::Int, L::Int, InCluster::BitMatrix, p::Float32, ZLattice::Array{Float32, 2}, pi32::Float32, rng::AbstractRNG)    # For Wolff update: add or not to the cluster the neighbors of a site already in the cluster
     NewCheckNeighbor = []
     Zsign = sign(ZLattice[i,j]-pi32/2)
     neighbor = [[mod(i-2,L)+1,j],[i,mod(j-2,L)+1],[mod(i,L)+1,j],[i,mod(j,L)+1]]    # The four neighbors
     for n=1:4                                       # To check the four neighbors
         k,l = neighbor[n]
-        if InCluster[k,l] == false && sign(ZLattice[k,l]-pi32/2) == Zsign && rand(TaskLocalRNG()) < p
+        if InCluster[k,l] == false && sign(ZLattice[k,l]-pi32/2) == Zsign && rand(rng) < p
             InCluster[k,l] = true
             push!(NewCheckNeighbor, [k,l])
         end
@@ -180,9 +183,9 @@ function AddNeighbors(i::Int,j::Int, L::Int, InCluster::BitMatrix, p::Float32, Z
     return NewCheckNeighbor, InCluster
 end
 
-function Swap(Replicas::Vector{Replica}, i::Int)    # For parallel tempering: try to swap with the next temperature
+function Swap(Replicas::Vector{Replica}, i::Int, rng_swap::AbstractRNG, βdiff_i::Float32)    # For parallel tempering: try to swap with the next temperature
     j=i+1
-    if (1/Replicas[i].T - 1/Replicas[j].T)*(Replicas[i].E - Replicas[j].E) > rand(TaskLocalRNG())
+    if βdiff_i*(Replicas[i].E - Replicas[j].E) > rand(rng_swap)
         Replicas[i].Lattice, Replicas[j].Lattice = Replicas[j].Lattice, Replicas[i].Lattice
         Replicas[i].E,       Replicas[j].E       = Replicas[j].E,       Replicas[i].E
         Replicas[i].σ,       Replicas[j].σ       = Replicas[j].σ,       Replicas[i].σ
@@ -192,25 +195,27 @@ function Swap(Replicas::Vector{Replica}, i::Int)    # For parallel tempering: tr
     end
 end
 
-function MHStep(step::Int, rep::Replica, L::Int, d::Float32, PBC::Bool, pi32::Float32)   # ful MH step: i.e. lattice sweep or Wolff algorithm, and (potentially) adjust the three σ for the three gaussian proposal to be closer to a 50% acceptance rate
-    Mz2 = mean(cos.(rep.Lattice[2,:,:]).^2)
+function MHStep(step::Int, rep::Replica, L::Int, d::Float32, PBC::Bool, pi32::Float32, rng::AbstractRNG)   # ful MH step: i.e. lattice sweep or Wolff algorithm, and (potentially) adjust the three σ for the three gaussian proposal to be closer to a 50% acceptance rate
+    Mz2 = Mag_z2(rep.Lattice, L)
     if mod(step,1000)==0 && Mz2 > .7f0
-        rep.Lattice, rep.E = MultipleIsingFlips(rep.Lattice, L, rep.T, pi32, d, PBC)    # Wolff algorithm
+        rep.Lattice, rep.E = MultipleIsingFlips(rep.Lattice, L, rep.T, pi32, d, PBC, rng)    # Wolff algorithm
     else
-        rep.Lattice, Accept, Tr = LatticeSweep(rep.Lattice, L, rep.E, rep.T, rep.σ, rep.σϕ, rep.σθ, pi32, PBC, d, IsingProba(Mz2), XYProba(Mz2))
-        if Tr[1] > 10
-            rep.σ = min(max(rep.σ*2*Accept[1]/Tr[1], 1/sqrt(step+1000)), 10)     # if the acceptance is higher than .5, sigma should increase (decreased) to explore more (less) the phasespace to get closer to .5
+        rep.Lattice, Accept, Tr = LatticeSweep(rep.Lattice, L, rep.T, rep.σ, rep.σϕ, rep.σθ, pi32, PBC, d, IsingProba(Mz2), XYProba(Mz2), rng)
+        @fastmath begin
+            if Tr[1] > 10
+                rep.σ = min(max(rep.σ*2*Accept[1]/Tr[1], 1/sqrt(step+1000)), 10)     # if the acceptance is higher than .5, sigma should increase (decreased) to explore more (less) the phasespace to get closer to .5
+            end
+            if Tr[3] > 0
+                a = 1/(Tr[3]+2)
+                rep.σϕ = min(max(rep.σϕ*2*min(max(Accept[3]/Tr[3], a), 1-a), 1/sqrt(step + 1000)), 10)     # if the acceptance is higher than .5, sigma should increase (decreased) to explore more (less) the phasespace to get closer to .5
+            end         # I could have used clamp(a,b,c) instead of min(max(a,c),b) but it is less efficient (around 50% slower)
+            if Tr[4] > 0
+                a = 1/(Tr[4]+2)
+                rep.σθ = min(max(rep.σθ*2*min(max(Accept[4]/Tr[4], a), 1-a), 1/sqrt(step+10000)), 2)     # if the acceptance is higher than .5, sigma should increase (decreased) to explore more (less) the phasespace to get closer to .5
+            end
+            rep.Acceptance += Accept
+            rep.Try += Tr
         end
-        if Tr[3] > 0
-            a = 1/(Tr[3]+2)
-            rep.σϕ = min(max(rep.σϕ*2*min(max(Accept[3]/Tr[3], a), 1-a), 1/sqrt(step + 1000)), 10)     # if the acceptance is higher than .5, sigma should increase (decreased) to explore more (less) the phasespace to get closer to .5
-        end         # I could have used clamp(a,b,c) instead of min(max(a,c),b) but it is less efficient (around 50% slower)
-        if Tr[4] > 0
-            a = 1/(Tr[4]+2)
-            rep.σθ = min(max(rep.σθ*2*min(max(Accept[4]/Tr[4], a), 1-a), 1/sqrt(step+10000)), 2)     # if the acceptance is higher than .5, sigma should increase (decreased) to explore more (less) the phasespace to get closer to .5
-        end
-        rep.Acceptance += Accept
-        rep.Try += Tr
     end
     return rep
 end
@@ -219,6 +224,7 @@ function MH_parallel_tempering(L::Int, N::Int, T::Vector{Float32}, burn::Int, pi
     nT = length(T)
     # Lattices = Array{Array{Float64,3}}(undef, nT, round(Int,2000/Skip))#-1)       # To save the last lattices
     Replicas = Vector{Replica}(undef, nT)
+    βdiff = 1 ./T[1:nT-1] - 1 ./ T[2:nT]    # inverse temperature difference, useful to swap lattices
     for i=1:nT                              # Initialize Replicas
         lat = Initial_lattice(L, pi32)
         Replicas[i] = Replica(lat, T[i], Energy(lat, L, PBC, d), .25f0, .4f0, .25f0, [0,0,0,0], [0,0,0,0])
@@ -232,14 +238,15 @@ function MH_parallel_tempering(L::Int, N::Int, T::Vector{Float32}, burn::Int, pi
     
     for i=1:Nswap
         Threads.@threads for n=1:nT
+            rng = TaskLocalRNG()
             for j=1:swap
                 k = (i-1)*swap+j
-                Replicas[n] = MHStep(k, Replicas[n], L, d, PBC, pi32)
+                Replicas[n] = MHStep(k, Replicas[n], L, d, PBC, pi32, rng)
                 if mod(k, Skip) == 0 && k > burn            # to not measure every lattice sweeps
                     lat = Replicas[n].Lattice
                     m = round(Int,(k-burn)/Skip)
                     Energies[n, m] = Energy(lat, L, PBC, d)
-                    Mag[n, m] = sqrt(mean(cos(phi)*sin(theta) for phi in lat[1,:,:], theta in lat[2,:,:])^2 + mean(sin(phi)*sin(theta) for phi in lat[1,:,:], theta in lat[2,:,:])^2 + mean(cos(theta) for theta in lat[2,:,:])^2)
+                    Mag[n, m] = mag(lat, L)
                     corr[n] += RowCorr(lat, L, PBC)
                     # if k > N-2000
                     #     kk = round(Int,(k+2000-N)/Skip)
@@ -250,7 +257,8 @@ function MH_parallel_tempering(L::Int, N::Int, T::Vector{Float32}, burn::Int, pi
             Replicas[n].E = Energy(Replicas[n].Lattice, L, PBC, d)  #   Only need to calculate the energy for storing (above) and for potentially swapping lattices
         end
         for n=1:nT-1
-            SwapAcceptance[n] += Swap(Replicas, n)
+            rng_swap = TaskLocalRNG()
+            SwapAcceptance[n] += Swap(Replicas, n, rng_swap, βdiff[n])
         end
     end
     SwapAccept = SwapAcceptance/Nswap
@@ -274,7 +282,7 @@ function RowCorr(Lattice::Array{Float32, 3}, L::Int, PBC::Bool)   # Return the a
     if PBC==false
         N = L-1
         corr = zeros(Float32, N)
-        @inbounds for i=1:N
+        @inbounds @fastmath for i=1:N
             a=0f0
             for j=1:L
                 for k=1:(L-i)
@@ -286,7 +294,7 @@ function RowCorr(Lattice::Array{Float32, 3}, L::Int, PBC::Bool)   # Return the a
     else
         N=round(Int, L/2-1)
         corr = zeros(Float32, N)
-        @inbounds for i=1:N
+        @inbounds @fastmath for i=1:N
             a=0f0
             for j=1:L
                 for k=1:L
@@ -300,16 +308,42 @@ function RowCorr(Lattice::Array{Float32, 3}, L::Int, PBC::Bool)   # Return the a
     return corr
 end
 
+function mag(Lattice::Array{Float32,3}, L::Int64)
+    X = 0f0
+    Y = 0f0
+    Z = 0f0
+    @inbounds @fastmath for j in 1:L, k in 1:L
+        stheta, ctheta = sincos(Lattice[2,j,k])
+        sphi, cphi = sincos(Lattice[1,j,k])
+        x = cphi*stheta
+        y = sphi*stheta
+        z = ctheta
+        X +=x
+        Y +=y
+        Z +=z
+    end
+    return sqrt(X^2+Y^2+Z^2)/L^2
+end
+
+function Mag_z2(Lattice::Array{Float32,3}, L::Int64)
+    magz2 = 0f0
+    @inbounds @fastmath for j=1:L, k=1:L
+        c = cos(Lattice[2,j,k])^2
+        magz2 += c*c
+    end
+    return magz2/L^2
+end
+
 @inline function Temperatures(Tmin::Float64, Tmax::Float64, N::Int)
     return Float32.(round.(Tmin .* (Tmax/Tmin) .^ ((0:N-1)/(N-1)); digits=3))
 end
 
 @inline @fastmath function IsingProba(Mz2::Float32)   # Probability of Ising-flip, just a choice, there is no perfect one
-    return (1.9f0*Mz2 .-.95f0).^2 .*(sign.(Mz2 .- .5f0).+1)/2
+    return (1.9f0*Mz2 -.95f0)^2 *(sign(Mz2 - .5f0)+1)/2
 end
 
 @inline @fastmath function XYProba(Mz2::Float32)      # Probability of XY-flip (either phi or theta, independently): useful to converge faster for XY config as σϕ and σθ are tuned independently, to really have θ very close to π/2
-    return (.9f0 .- 9f0*Mz2).^2 .*(sign.(.1f0 .-Mz2).+1)/2
+    return (.9f0 - 9f0*Mz2)^2 *(sign(.1f0 -Mz2)+1)/2
 end # just a choice, there is no perfect one
 
 
